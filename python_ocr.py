e9ea474386a4a6d294350f6d5c30b9530b2d1b10
Roberto Gomes <91014834+jr-roberto@users.noreply.github.com>
2024-07-01 16:02:57 -0300

    Update python_ocr.py

--- python_ocr.py
+++ python_ocr.py
@@ -1,5 +1,5 @@
 from pytesseract import pytesseract
-pytesseract.tesseract_cmd = r'C://Users//roberto.silva//AppData//Local//Programs//Tesseract-OCR//tesseract.exe'
+pytesseract.tesseract_cmd = r'<CAMINHO_DO_EXECUTAVEL_TESSERACT>//tesseract.exe'
 
 imagem_1="imagem_1.png"
 imagem_2="imagem_2.png"
